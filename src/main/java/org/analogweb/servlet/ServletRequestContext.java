312f4ac469a81cb8188cc97d041ae1fd0ab50e1c
snowgoose.yk <snowgoose.yk@gmail.com>
2013-01-11 05:42:23 +0900

    first commit

---
+++ src/main/java/org/analogweb/servlet/ServletRequestContext.java
@@ -0,0 +1,15 @@
+package org.analogweb.servlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.analogweb.RequestContext;
+
+public interface ServletRequestContext extends RequestContext {
+
+    HttpServletRequest getServletRequest();
+    HttpServletResponse getServletResponse();
+    ServletContext getServletContext();
+
+}
